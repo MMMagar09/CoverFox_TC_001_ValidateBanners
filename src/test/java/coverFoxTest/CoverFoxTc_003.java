82ffa3eabaa50709372b3f99f538ef0fb80d45e2
vrushali-bhagat <bhagatvru97@gmail.com>
2025-01-20 13:59:04 +0530

    [vrushali-bhagat] some changes

---
+++ src/test/java/coverFoxTest/CoverFoxTc_003.java
@@ -0,0 +1,14 @@
+package coverFoxTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CoverFoxTc_003 {
+	
+	@Test
+	public void testCase_003()
+	{
+		Reporter.log("add some lines", true);
+	}
+
+}
